49a44a3a4a80bbdca41118b468cce66f9045ba3e
gabrielvieira3 <gabriel.fsv@hotmail.com>
2021-09-08 20:08:48 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dsvendas.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsvendas.dto.SellerDTO;
+import com.devsuperior.dsvendas.service.SellerService;
+
+@RestController
+//Indica o caminho
+@RequestMapping(value = "/sellers")
+public class SellerController {
+	
+	@Autowired
+	private SellerService service;
+	
+	//ENDPOINT -> chama o caminho la no navegador
+	@GetMapping
+	public ResponseEntity<List<SellerDTO>> findAll() {
+		List<SellerDTO> list = service.findAll();
+		return ResponseEntity.ok(list);
+	}
+	
+}
