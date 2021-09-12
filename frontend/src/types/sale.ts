d40972c87bf8e33023c4379a5cc55db925bcc57b
gabrielvieira3 <gabriel.fsv@hotmail.com>
2021-09-12 15:55:26 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,27 @@
+import { Seller } from "./seller"
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    totalPages: number;
+    totalElements: number;
+    last: boolean;
+    first: boolean;
+    numberOfElements?: number;
+    size?: number;
+    number: number;
+    empty?: boolean;
+}
+
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
